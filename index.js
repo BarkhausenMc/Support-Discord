24f251beea3bd95e451f420647746d773fc609dd
Flo <you@example.com>
2026-09-18 18:35:24 +0200

    v5.5 deisgn

--- index.js
+++ index.js
@@ -104,7 +104,7 @@ client.on('messageCreate', async (message) => {
             const categoryContainer = new ContainerBuilder()
             .addTextDisplayComponents(
                 new TextDisplayBuilder()
-                    .setContent('# `📩` Ticket Erstellen')
+                    .setContent('# `📩` Ticket Erstellen ║ ||* Drücke ein Button, um ein Tickt zu erstellen. *||')
             );
 
             const buttonContainer = new ContainerBuilder()

00bcc84d27d6bd071b0149d515cd60ff70f1180c
Flo <you@example.com>
2026-09-20 14:51:07 +0200

    test ticket erstellen

--- index.js
+++ index.js
@@ -31,7 +31,6 @@ const client = new Client({
 
 const userIdToPostId = new Map(); 
 const postIdToUserId = new Map(); 
-const userIdToTicketPanel = new Map();
 
 
 
@@ -326,7 +325,7 @@ client.on('interactionCreate', async (interaction) => {
 
             // Post erstellen
             const post = await targetChannel.threads.create({
-                name: `🎫 ${subject}`,
+                name: `${interaction.user.tag}`,
                 message: { content: ticketText },
                 reason: `Ticket von ${interaction.user.tag}`
             });
@@ -334,50 +333,6 @@ client.on('interactionCreate', async (interaction) => {
             // Maps pflegen
             userIdToPostId.set(interaction.user.id, post.id);
             postIdToUserId.set(post.id, interaction.user.id);
-            
-            // Ticket-Panel finden und Buttons deaktivieren
-            const panelMessageId = userIdToTicketPanel.get(interaction.user.id);
-
-            if (panelMessageId) {
-                try {
-                    const panelMessage = await interaction.channel.messages.fetch(panelMessageId);
-
-                    const disabledButtonContainer = new ActionRowBuilder()
-                        .addComponents(
-                            new ButtonBuilder()
-                                .setCustomId('generel_support')
-                                .setLabel('❓ Generell Support')
-                                .setStyle(ButtonStyle.Secondary)
-                                .setDisabled(true),
-
-                            new ButtonBuilder()
-                                .setCustomId('cooperation')
-                                .setLabel('🤝 Kooperation')
-                                .setStyle(ButtonStyle.Secondary)
-                                .setDisabled(true),
-
-                            new ButtonBuilder()
-                                .setCustomId('staff_apply')
-                                .setLabel('📝 Staff Bewerbung')
-                                .setStyle(ButtonStyle.Secondary)
-                                .setDisabled(true)
-                        );
-
-                    await panelMessage.edit({
-                        components: [
-                            pictureContainer,
-                            categoryContainer,
-                            disabledButtonContainer
-                        ],
-                        flags: MessageFlags.IsComponentsV2
-                    });
-
-                } catch (error) {
-                    console.error('❌ Konnte Ticket-Buttons nicht deaktivieren:', error.message);
-                }
-
-                userIdToTicketPanel.delete(interaction.user.id);
-            }
 
 
             // Bestätigung an User
